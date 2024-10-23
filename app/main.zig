c30498141557a42b122d4c882d077fab6779d72e
fchamp <filipmhpersson@gmail.com>
2024-10-23 19:25:05 +0200

    Squash merge test into master

--- app/main.zig
+++ app/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
+const reader = @import("streamreader.zig");
 const stdout = std.io.getStdOut().writer();
-const allocator = std.heap.page_allocator;
+var allocator = std.heap.page_allocator;
 
 pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
@@ -13,18 +14,19 @@ pub fn main() !void {
 
     const command = args[1];
 
+    var position: usize = 0;
     if (std.mem.eql(u8, command, "decode")) {
         const encodedStr = args[2];
-        const decodedStr = decodeBencode(encodedStr) catch {
+        const b = reader.getNextValue(encodedStr, &position, allocator) catch {
             try stdout.print("Invalid encoded value\n", .{});
             std.process.exit(1);
         };
-        try printBencode(decodedStr);
+        try printBencode(b);
         try stdout.print("\n", .{});
     }
 }
 
-fn printBencode(bencodedValue: BencodeValue) !void {
+fn printBencode(bencodedValue: reader.BencodeValue) !void {
     switch (bencodedValue) {
         .string => {
             var string = std.ArrayList(u8).init(allocator);
@@ -32,15 +34,32 @@ fn printBencode(bencodedValue: BencodeValue) !void {
             const jsonStr = try string.toOwnedSlice();
             try stdout.print("{s}", .{jsonStr});
         },
+        .dictionary => {
+            if(!@inComptime()) {
+
+            try stdout.print("{{", .{});
+            var k = bencodedValue.dictionary.keyIterator();
+            while(k.next()) |key| {
+
+                var string = std.ArrayList(u8).init(allocator);
+                try std.json.stringify(key.*, .{}, string.writer());
+                const jsonStr = try string.toOwnedSlice();
+                try stdout.print("{s}", .{jsonStr});
+                try stdout.print(":", .{});
+                try printBencode(bencodedValue.dictionary.get(key.*).?);
+            }
+            try stdout.print("}}", .{});
+            }
+        },
         .int => {
             try stdout.print("{d}", .{bencodedValue.int});
         },
         .array => {
             try stdout.print("[", .{});
 
-            for (bencodedValue.array, 0..) |item, i| {
+            for (bencodedValue.array.*, 0..) |item, i| {
                 try printBencode(item);
-                if(i < bencodedValue.array.len - 1) {
+                if(i < bencodedValue.array.*.len - 1) {
                     try stdout.print(", ", .{});
                 }
             }
@@ -48,100 +67,3 @@ fn printBencode(bencodedValue: BencodeValue) !void {
         },
     }
 }
-fn decodeBencode(encodedValue: []const u8) !BencodeValue {
-    std.debug.print("Decoding {s}\n", .{encodedValue});
-    if (encodedValue[0] >= '0' and encodedValue[0] <= '9') {
-        const firstColon = std.mem.indexOf(u8, encodedValue, ":");
-        if (firstColon == null) {
-            return error.InvalidArgument;
-        }
-        return BencodeValue{ .string = encodedValue[firstColon.? + 1 ..] };
-    } else if (encodedValue[0] == 'i' ) {
-        if(encodedValue[encodedValue.len - 1] != 'e') {
-            std.debug.print("Expected {d} but got {d}\n", .{'e', encodedValue[encodedValue.len - 1]});
-            return error.InvalidArgument;
-        }
-        var intValue: i64 = 0;
-
-        if(encodedValue[1] == '-') {
-            for(encodedValue[2..encodedValue.len-1]) |char| {
-                intValue = (intValue * 10) - (char - '0');
-            }
-        } else {
-            for(encodedValue[1..encodedValue.len-1]) |char| {
-                intValue = (intValue * 10) + char - '0';
-            }
-        }
-        return BencodeValue{ .int = intValue };
-    } else if(encodedValue[0] == 'l' and encodedValue[encodedValue.len - 1] == 'e') {
-
-        var i:usize = 1;
-        var bencodedArray = std.ArrayList(BencodeValue).init(allocator);
-
-        std.debug.print("Parsing bencoded list", .{});
-        while(i < encodedValue.len - 1) {
-            const curr = encodedValue[i];
-            std.debug.print("Iterating encoded value at index {d} with value {d}\n", .{ i,  curr});
-            if(curr >= '0' and curr <= '9') {
-                var firstColon = std.mem.indexOf(u8, encodedValue[i..], ":")  orelse unreachable;
-                firstColon += i;
-                const len = getInt(encodedValue[i..firstColon]);
-                std.debug.print("For text {s} Parsing string colon  {d}, with len {d} and index {d}\n", .{ encodedValue, firstColon, len, i});
-                const decodedText = try decodeBencode(encodedValue[i..len + firstColon + 1]);
-                try bencodedArray.append(decodedText);
-                i = firstColon + len + 1;
-
-            } else if (curr == 'i'){
-
-                const firstEnd = std.mem.indexOf(u8, encodedValue[i..], "e");
-                
-                if (firstEnd) |end| {
-                    std.debug.print("Parsing int with end {d}\n", .{ end });
-                    const obj = try decodeBencode(encodedValue[i..end + i + 1]);
-                    try bencodedArray.append(obj);
-                    i += end + 1;
-                } else {
-                    try stdout.print("Cannot find end for int in array\n", .{});
-                    std.process.exit(1);
-                }
-            } 
-            else if (curr == 'l') {
-                const innerListIndex = std.mem.lastIndexOf(u8, encodedValue[i..encodedValue.len - 1], "ee") orelse unreachable;
-                std.debug.print("Found inner list, end index {d}\n", .{innerListIndex});
-
-                const obj = try decodeBencode(encodedValue[i..innerListIndex + i + 2]);
-                try bencodedArray.append(obj);
-                std.debug.print("Done inner", .{});
-                i += innerListIndex + 2;
-            }
-            else {
-                    try stdout.print("Only strings and ints allowed in arrays\n", .{});
-                    std.process.exit(1);
-            }
-        }
-            const slice = try bencodedArray.toOwnedSlice();
-            return BencodeValue{.array = slice};
-    }
-    else {
-        try stdout.print("Only strings are supported at the moment, you send value {d}\n", .{encodedValue[0]});
-        std.process.exit(1);
-    }
-}
-
-fn getInt(digits: [] const u8) usize {
-
-    var int:usize = 0;
-    for(digits) |char| {
-        int = (int * 10) + char - '0';
-    }
-    return int;
-
-}
-
-
-const BencodedType = enum { int, string, array };
-const BencodeValue = union(BencodedType){
-    int: i64,
-    string: []const u8,
-    array: []const BencodeValue
-};
---
+++ app/streamreader.zig
@@ -0,0 +1,143 @@
+const std = @import("std");
+
+pub fn getNextValue(input: []const u8, pos: *usize, allocator: std.mem.Allocator) ParserError!BencodeValue {
+    switch (input[pos.*]) {
+        'i' => {
+            return getInteger(input, pos);
+        },
+        'l' => {
+            return getList(input, pos, allocator);
+        },
+        'd' => {
+            return getDictionary(input, pos, allocator);
+        },
+        '0'...'9' => {
+            return getString(input, pos);
+        },
+        else => {
+            std.log.err("Failed parsing {s} with pos {d}", .{ input, pos.* });
+            return ParserError.InvalidArgument;
+        },
+    }
+}
+
+fn getInteger(input: []const u8, pos: *usize) ParserError!BencodeValue {
+    pos.* += 1;
+    var end = std.mem.indexOf(u8, input[pos.*..], "e") orelse return ParserError.InvalidArgument;
+
+    end += pos.*;
+    var intValue: i64 = 0;
+
+    if (input[pos.*] == '-') {
+        for (input[pos.* + 1 .. end]) |char| {
+            intValue = (intValue * 10) - (char - '0');
+        }
+    } else {
+        for (input[pos.*..end]) |char| {
+            intValue = (intValue * 10) + char - '0';
+        }
+    }
+    pos.* = end + 1;
+    return BencodeValue{ .int = intValue };
+}
+fn getString(input: []const u8, pos: *usize) ParserError!BencodeValue {
+    var firstColon = std.mem.indexOf(u8, input[pos.*..], ":") orelse unreachable;
+    firstColon += pos.*;
+    const len = getInt(input[pos.*..firstColon]);
+
+    pos.* = firstColon + len + 1;
+    return BencodeValue{ .string = input[firstColon + 1 .. pos.*] };
+}
+fn getList(input: []const u8, pos: *usize, allocator: std.mem.Allocator) ParserError!BencodeValue {
+    const lastEnd = std.mem.lastIndexOf(u8, input, "e") orelse return ParserError.InvalidArgument;
+    var bencodedArray = std.ArrayList(BencodeValue).init(allocator);
+    defer bencodedArray.deinit();
+    pos.* += 1;
+    while (pos.* < lastEnd) {
+        const b = try getNextValue(input, pos, allocator);
+        bencodedArray.append(b) catch return ParserError.AllocatorError;
+    }
+    const a = bencodedArray.toOwnedSlice() catch return ParserError.AllocatorError;
+    pos.* += 1;
+    return BencodeValue{ .array = &a };
+}
+
+fn getDictionary(input: []const u8, pos: *usize, allocator: std.mem.Allocator) ParserError!BencodeValue {
+    const lastEnd = std.mem.lastIndexOf(u8, input, "e") orelse return ParserError.InvalidArgument;
+    pos.* += 1;
+    var dict = std.StringHashMap(BencodeValue).init(allocator);
+
+    while (pos.* < lastEnd) {
+        const key = try getString(input, pos);
+        const value = try getNextValue(input, pos, allocator);
+        dict.put(key.string, value) catch return ParserError.AllocatorError;
+    }
+
+    pos.* += 1;
+    return BencodeValue{ .dictionary = dict };
+}
+
+fn getInt(digits: []const u8) usize {
+    var int: usize = 0;
+    for (digits) |char| {
+        int = (int * 10) + char - '0';
+    }
+    return int;
+}
+pub const BencodedType = enum { int, string, array,dictionary };
+pub const BencodeValue = union(BencodedType) {
+    int: i64,
+    string: []const u8,
+    array: *const []const BencodeValue,
+    dictionary: std.StringHashMap(BencodeValue),
+};
+pub const ParserError = error{ InvalidArgument, AllocatorError };
+
+test "Parse string return string" {
+    const alloc = std.testing.allocator;
+    const testValue = "6:catmat";
+    var pos: usize = 0;
+    const result = try getNextValue(testValue, &pos, alloc);
+    try std.testing.expectEqualStrings(result.string, "catmat");
+    try std.testing.expectEqual(pos, testValue.len);
+}
+test "Parse int return int" {
+    const alloc = std.testing.allocator;
+    const testValue = "i3232e";
+    var pos: usize = 0;
+    const result = try getNextValue(testValue, &pos, alloc);
+    try std.testing.expectEqual(result.int, 3232);
+    try std.testing.expectEqual(pos, testValue.len);
+}
+test "Parse list with intreturn int" {
+    const alloc = std.testing.allocator;
+    const testValue = "li3232ee";
+    var pos: usize = 0;
+    const arr = try getNextValue(testValue, &pos, alloc);
+    defer alloc.free(arr.array.*);
+    try std.testing.expectEqual(arr.array.*[0].int, 3232);
+    try std.testing.expectEqual(pos, testValue.len);
+}
+
+test "Parse list with int and string" {
+    const alloc = std.testing.allocator;
+    const testValue = "li3232e10:piineapplee";
+    var pos: usize = 0;
+    const arr = try getNextValue(testValue, &pos, alloc);
+
+    try std.testing.expectEqual(arr.array.*[0].int, 3232);
+    try std.testing.expectEqual(arr.array.*.len, 2);
+    try std.testing.expectEqual(pos, testValue.len);
+    defer alloc.free(arr.array.*);
+}
+test "Parse dict with int and string" {
+    const alloc = std.testing.allocator;
+    const testValue = "d10:piineapplei3232ee";
+    var pos: usize = 0;
+    var b = try getNextValue(testValue, &pos, alloc);
+
+    const res = b.dictionary.get("piineapple").?;
+    defer b.dictionary.clearAndFree(); 
+    try std.testing.expectEqual(res.int, 3232);
+    try std.testing.expectEqual(pos, testValue.len);
+}
