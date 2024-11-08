d711b07f296495fdfad083caa240adfc22e00061
fchamp <filipmhpersson@gmail.com>
2024-11-08 22:46:32 +0100

    codecrafters submit [skip ci]

--- app/main.zig
+++ app/main.zig
@@ -65,6 +65,25 @@ pub fn main() !void {
         try getTorrent(b, alloca);
         try stdout.print("\n", .{});
     }
+
+    if (std.mem.eql(u8, command, "handshake")) {
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+        const alloca = arena.allocator();
+
+        const fileArg = args[2];
+        const peer = args[3];
+        var file = try std.fs.cwd().openFile(fileArg, .{});
+        const encodedStr = try file.readToEndAlloc(allocator, 1024 * 1024);
+        const b = reader.getNextValue(encodedStr, &position, alloca) catch {
+            try stdout.print("Invalid encoded value\n", .{});
+            std.process.exit(1);
+        };
+        defer file.close();
+
+        try getHandshake(b, peer, alloca);
+        try stdout.print("\n", .{});
+    }
 }
 
 fn printBencode(bencodedValue: reader.BencodeValue) !void {
@@ -137,8 +156,6 @@ fn printTorrent(input: reader.BencodeValue, alloc: std.mem.Allocator) !void {
     var sha1: [20]u8 = undefined;
     sha.hash(bencodedInfo, &sha1, sha.Options{});
 
-
-
     try stdout.print("Tracker URL: {s}\n", .{url.string});
     try stdout.print("Length: {d}\n", .{length.int});
     try stdout.print("Info Hash: ", .{});
@@ -148,8 +165,8 @@ fn printTorrent(input: reader.BencodeValue, alloc: std.mem.Allocator) !void {
     {
         var i: usize = 0;
         while (i < pieces.len) {
-            try stdout.print("{s}\n", .{std.fmt.fmtSliceHexLower(pieces[i..i+20])});
-            i +=20;
+            try stdout.print("{s}\n", .{std.fmt.fmtSliceHexLower(pieces[i .. i + 20])});
+            i += 20;
         }
     }
 }
@@ -172,58 +189,55 @@ fn getTorrent(input: reader.BencodeValue, alloc: std.mem.Allocator) !void {
 
     var sha1: [20]u8 = undefined;
     sha.hash(bencodedInfo, &sha1, sha.Options{});
-    var client = std.http.Client {.allocator =  alloc };
+    var client = std.http.Client{ .allocator = alloc };
     const length = metadata.dictionary.get("length").?;
-    const downloaded:usize = 0;
-    const uploaded:usize = 0;
+    const downloaded: usize = 0;
+    const uploaded: usize = 0;
     defer client.deinit();
     var list = std.ArrayList(u8).init(alloc);
 
-
     const infohash = try std.fmt.allocPrint(alloc, "{s}", .{std.fmt.fmtSliceHexLower(sha1[0..])});
-
 
     try list.appendSlice(url.string);
     try list.appendSlice("?info_hash=");
     {
         var i: usize = 0;
-        while(i < infohash.len) : (i += 2) {
-            try list.appendSlice(try std.fmt.allocPrint(alloc, "%{s}", .{infohash[i..i+2]}));
+        while (i < infohash.len) : (i += 2) {
+            try list.appendSlice(try std.fmt.allocPrint(alloc, "%{s}", .{infohash[i .. i + 2]}));
         }
     }
     try list.appendSlice("&peer_id=homersimpsontvcatgoo");
     try list.appendSlice("&port=6881");
-    try list.appendSlice(try std.fmt.allocPrint(alloc, "&uploaded={d}", .{ uploaded}));
-    try list.appendSlice(try std.fmt.allocPrint(alloc, "&downloaded={d}", .{ downloaded}));
-        try list.appendSlice(try std.fmt.allocPrint(alloc, "&left={d}", .{ length.int}));
+    try list.appendSlice(try std.fmt.allocPrint(alloc, "&uploaded={d}", .{uploaded}));
+    try list.appendSlice(try std.fmt.allocPrint(alloc, "&downloaded={d}", .{downloaded}));
+    try list.appendSlice(try std.fmt.allocPrint(alloc, "&left={d}", .{length.int}));
     try list.appendSlice("&compact=1");
-    const fmtUrl  = try list.toOwnedSlice();
+    const fmtUrl = try list.toOwnedSlice();
 
     std.debug.print("Request URL {s}\n", .{fmtUrl});
-    const uri =  try std.Uri.parse(fmtUrl);
+    const uri = try std.Uri.parse(fmtUrl);
 
     std.debug.print("Request parsed URL {s}\n", .{uri.query.?.percent_encoded});
     var headerbuf: [1000]u8 = undefined;
-    const options = std.http.Client.RequestOptions { .server_header_buffer = &headerbuf };
-    var con = try client.open(std.http.Method.GET, uri,options );
+    const options = std.http.Client.RequestOptions{ .server_header_buffer = &headerbuf };
+    var con = try client.open(std.http.Method.GET, uri, options);
     defer con.deinit();
     try con.send();
     try con.wait();
     const resp = con.response;
     const buf = try alloc.alloc(u8, resp.content_length.?);
     _ = try con.readAll(buf);
-    std.debug.print("HTTP Response {s}, code {d}\n", .{buf,resp.status});
-    var position:usize = 0;
+    std.debug.print("HTTP Response {s}, code {d}\n", .{ buf, resp.status });
+    var position: usize = 0;
     const response = try reader.getNextValue(buf, &position, alloc);
     const p = response.dictionary.get("peers").?.string;
-    
     {
         var i: usize = 0;
-        while(i < p.len) : (i += 6) {
-            const slice = p[i..i+6];
+        while (i < p.len) : (i += 6) {
+            const slice = p[i .. i + 6];
 
             const aw = std.fmt.fmtSliceHexLower(slice[4..]).data;
-            var ba : [8]u8 = undefined;
+            var ba: [8]u8 = undefined;
             ba[0] = 0;
             ba[1] = 0;
             ba[2] = 0;
@@ -234,20 +248,72 @@ fn getTorrent(input: reader.BencodeValue, alloc: std.mem.Allocator) !void {
             ba[7] = aw[1];
             const parsedPort = std.mem.readInt(usize, &ba, std.builtin.Endian.big);
 
-            try stdout.print("{d}.{d}.{d}.{d}:{d}\n", .{
-                slice[0],
-                slice[1],
-                slice[2],
-                slice[3],
-                parsedPort
-            });
+            try stdout.print("{d}.{d}.{d}.{d}:{d}\n", .{ slice[0], slice[1], slice[2], slice[3], parsedPort });
         }
     }
-
-
-
 }
 
+fn getHandshake(input: reader.BencodeValue, sampleIp: []u8, alloc: std.mem.Allocator) !void {
+    if (input != reader.BencodedType.dictionary) {
+        return error.InvalidArgument;
+    }
+
+    const metadata = input.dictionary.get("info").?;
+
+    if (metadata != reader.BencodedType.dictionary) {
+        return error.InvalidArgument;
+    }
+
+    //const pieceLength = metadata.dictionary.get("piece length").?;
+    //const pieces = metadata.dictionary.get("pieces").?.string;
+    const bencodedInfo = try writer.bencode(&metadata, alloc);
+
+    var sha1: [20]u8 = undefined;
+    sha.hash(bencodedInfo, &sha1, sha.Options{});
+
+
+    const indexSemi = std.mem.indexOf(u8, sampleIp, ":").?;
+
+    var port:u16 = 0;
+    for(sampleIp[indexSemi+1..]) |char| {
+        std.debug.print("Char in port {c}\n", .{char});
+        port = port * 10 + (char-'0');
+    }
+
+    const ip = try std.net.Ip4Address.parse(sampleIp[0..indexSemi], port);
+
+    const c = std.net.Address{ .in = ip };
+    
+    const conn = try std.net.tcpConnectToAddress(c);
+
+    std.debug.print("Post close\n",.{});
+    defer conn.close();
+
+    const tcpReader = conn.reader();
+    const tcpwriter = conn.writer();
+
+    const handshake = Handshake { .infoHash = sha1, .peerId = "00112233445566778891".*};
+    try tcpwriter.writeStruct(handshake);
+
+    const res = try tcpReader.readStruct(Handshake);
+    std.debug.print("Read response {s} buff length ", .{res.peerId[0..]});
+     try stdout.print("Peer ID: {s}", .{std.fmt.bytesToHex(res.peerId[0..])});
+}
+
+
+const Handshake = extern struct {
+
+    protocolLength: u8 = 19,
+
+    ident: [19]u8 = "BitTorrent protocol".*,
+
+    reserved: [8]u8 = std.mem.zeroes([8]u8),
+
+    infoHash: [20]u8,
+
+    peerId: [20]u8,
+
+};
 fn lessthan(_: void, lhs: []const u8, rhs: []const u8) bool {
     return std.mem.order(u8, lhs, rhs) == .lt;
 }
